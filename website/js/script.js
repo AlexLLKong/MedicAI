c3cc09ef05384294dcf41aaae1ec97a12d976c81
Towa Quimbayo <towacurtis@hotmail.com>
2022-03-13 08:12:16 -0700

    add symptoms functional

--- website/js/script.js
+++ website/js/script.js
@@ -1,31 +1,32 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     // Opens hamburger menu
     function openNav() {
         document.getElementById("myNav").style.width = "100%";
     }
+    $('#analyzeSympBtn').attr('disabled', 'disabled');
 });
 
 $(document).ready(function () {
-    if(!localStorage.SessionID){
-      console.log("No session id in local storage");
-      fetch("/init")
-        .then(response => response.json())
-        .then(jsonData => {
+    if (!localStorage.SessionID) {
+        console.log("No session id in local storage");
+        fetch("/init")
+            .then(response => response.json())
+            .then(jsonData => {
 
-        localStorage.setItem("SessionID", jsonData.SessionID);
-      })
+                localStorage.setItem("SessionID", jsonData.SessionID);
+            })
     } else {
-      fetch("/acceptTerms", {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          SessionID: localStorage.SessionID
+        fetch("/acceptTerms", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                SessionID: localStorage.SessionID
+            })
         })
-        })
-        .then(response => response.json())
-        .then(jsonData => console.log(jsonData))
+            .then(response => response.json())
+            .then(jsonData => console.log(jsonData))
     }
     $(document).scroll(function () {
         var $nav = $(".navbar-custom");
@@ -37,8 +38,8 @@ $(document).ready(function () {
         $('.me-navbar-icon-animated').toggleClass('open');
     });
 
-    $('.me-terms-check:checkbox').change(function(){
-        if($(this).is(":checked")) {
+    $('.me-terms-check:checkbox').change(function () {
+        if ($(this).is(":checked")) {
             $('.me-terms-btn .me-btn-primary').removeClass("me-disabled");
             $(".me-terms-btn .me-btn-primary").attr("href", "/html/home.html")
         } else {
@@ -74,6 +75,7 @@ document.querySelector('a[href="#addSymptom"]')
 // offCanvasMenu.close();
 
 // Age range slider
+
 const range2 = document.getElementById('ageRangeValue');
 const rangeV2 = document.getElementById('rangeAgeCount');
 
@@ -91,26 +93,78 @@ function setSymptomData(id) {
     alert(id);
     localStorage.setItem('sympID', id);
     getSymptomData(id);
+    setUserResponse(id);
+    createSymptomTemplate(id);
 }
 
-function getSymptomData(id) {
-    $.getJSON("./js/SymptomsOutput.json", function(data) {
+function analyzeResults() {
+    
+}
+
+function setUserResponse(id) {
+    $.getJSON("./js/SymptomsOutput.json", function (data) {
         $.each(data, function (key, val) {
-            if(val.name == id) {
-                
+            if (val.name == id) {
+                var x = $('.nextStep').attr('value', 'Add Symptoms');
+                if (val.min >= 0) {
+                    $(x).click(function () {
+                        var userChoice = document.getElementById("numSlider").innerHTML;
+                        localStorage.setItem('sympChoiceSlider', userChoice);
+                    });
+
+                } else {
+                    $(x).click(function () {
+                        var userChoiceDrop = document.getElementById("symptomResponse").value;
+                        localStorage.setItem('sympChoiceDrop', userChoiceDrop);
+                    });
+                }
+            }
+        });
+    });
+}
+
+function createSymptomTemplate(id) {
+    $.getJSON("./js/SymptomsOutput.json", function (data) {
+        var output;
+        $.each(data, function (key, val) {
+            if (val.name == id) {
+                output = '<div class="me-symptom"><div class="me-symptom-name">' + val.laytext;
+                output += '<span class="me-symptom-response"><span class="me-symptom-response"style="font-weight:600;">(';
+
+                if (val.min >= 0) {
+                    output += localStorage.getItem('sympChoiceSlider');
+
+                } else {
+                    output += localStorage.getItem('sympChoiceDrop');
+                }
+
+                output += ')</span></div><div class="me-symptom-btns"><a class="btn me-btn-primary" href="#" role="button">Edit</a><a class="btn me-btn-secondary" href="#" role="button">Delete</a></div>';
+            }
+        });
+
+        $('.me-symptoms').html(output);
+    });
+}
+
+
+function getSymptomData(id) {
+    $.getJSON("./js/SymptomsOutput.json", function (data) {
+        $.each(data, function (key, val) {
+            if (val.name == id) {
+
                 var v = val.laytext;
                 document.querySelector(".symptomName").innerHTML = v;
-                
-                if(val.choices != null) {
+
+                if (val.choices != null) {
                     var output = '<select name="symptomResponse" id="symptomResponse" required="required">';
                     var count = 0;
 
-                    $.each(val.choices, function() {
-                            console.log(v);
-                            output += '<option class="sympChoices">';
-                            output += val.choices[count].text;
-                            output += '</option>';
-                            count++;
+                    $.each(val.choices, function () {
+                        console.log(v);
+                        output += '<option class="sympChoices">';
+                        output += val.choices[count].text;
+                        output += '</option>';
+                        count++;
                     });
 
                     output += '</select>';
@@ -119,8 +173,8 @@ function getSymptomData(id) {
                     console.log(val.min);
 
                     var output = '<div class="range-value2" id="rangeV"></div><input type="range" class="form-range" id="age" name="age" ';
-                    
-                    output += 'min="' + val.min + '" max="'+ val.max + '"/>';
+
+                    output += 'min="' + val.min + '" max="' + val.max + '"/>';
                     $('#symptomResponses').html(output);
                     const range = document.getElementById('age');
                     const rangeV = document.getElementById('rangeV');
@@ -137,9 +191,22 @@ function getSymptomData(id) {
                     console.log("Choices doesnt exist!");
                 }
 
+                var x = $('.nextStep').attr('value', 'Add Symptoms');
+                if (val.min >= 0) {
+                    $(x).click(function () {
+                        var userChoice = document.getElementById("numSlider").innerHTML;
+                        localStorage.setItem('sympChoiceSlider', userChoice);
+                    });
+
+                } else {
+                    $(x).click(function () {
+                        var userChoiceDrop = document.getElementById("symptomResponse").value;
+                        localStorage.setItem('sympChoiceDrop', userChoiceDrop);
+                    });
+                }
             }
         });
-    }); 
+    });
 }
 
 
@@ -147,13 +214,13 @@ function getSymptomData(id) {
 $(document).ready(function () {
     var length = $("section").length - 1;
     var child = 1;
-    
+
     $("section").not("section:nth-of-type(1)").hide();
-    $("section").not("section:nth-of-type(1)").css('transform','translateX(100px)');
+    $("section").not("section:nth-of-type(1)").css('transform', 'translateX(100px)');
 
     $(".nextStep").click(function () {
         var id = $(this).attr("id");
-        
+
         if (id == "next") {
             if (child <= length) {
                 child++;
@@ -167,10 +234,10 @@ $(document).ready(function () {
         }
         var currentSection = $("section:nth-of-type(" + child + ")");
         currentSection.fadeIn();
-        currentSection.css('transform','translateX(0)');
-        currentSection.prevAll('section').css('transform','translateX(-100px)');
-        currentSection.nextAll('section').css('transform','translateX(100px)');
-    
+        currentSection.css('transform', 'translateX(0)');
+        currentSection.prevAll('section').css('transform', 'translateX(-100px)');
+        currentSection.nextAll('section').css('transform', 'translateX(100px)');
+
         $('section').not(currentSection).hide();
     });
 
@@ -182,7 +249,7 @@ $(document).ready(function () {
         }
     });
 
-    $.getJSON("./js/SymptomsOutput.json", function(data) {
+    $.getJSON("./js/SymptomsOutput.json", function (data) {
         console.log(data);
         $('#txt-search').keyup(function () {
             var searchField = $(this).val();
@@ -202,7 +269,7 @@ $(document).ready(function () {
                     output += '<div id="' + sympID + '" onclick="setSymptomData(this.id)">' + val.laytext + '</div>'
                     output += '</div>';
                     count++;
-                }               
+                }
 
             });
             output += '</div><span style="padding-bottom: 10px;"></span>';
